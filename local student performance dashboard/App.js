1f5e8efd8c72146e67734b262118b3ad1a2beaa5
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-04-26 18:17:29 +0530

    Create App.js

---
+++ local student performance dashboard/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import StudentDashboard from './StudentDashboard'; // import the new component
+
+function App() {
+  return (
+    <div className="App">
+      <StudentDashboard />
+    </div>
+  );
+}
+
+export default App;
